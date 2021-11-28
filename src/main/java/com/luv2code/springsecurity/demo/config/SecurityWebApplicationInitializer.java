7ff3b8dfb8658abb6af54c780bb0a9eedf90990a
Muhammad Irfan <muhammad.irfan@praweda.id>
2021-11-28 14:42:23 +0700

    initial commit

---
+++ src/main/java/com/luv2code/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,13 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * 
+ * @author muhammad irfan
+ * Special class to register the Spring Security Filters
+ *
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
